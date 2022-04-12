e4f198db2622fdaf0361a010ca75782799a50d48
niemal <niemal@gmx.com>
2022-04-13 00:17:16 +0300

    added animation on posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,13 +4,20 @@ import Sub from '../../components/sub';
 import styles from '../../styles/Posts.module.css';
 import Image from 'next/image';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import { animated, useTransition } from 'react-spring';
 
 export default function Post({ postData, siteUrl }) {
+    let props = {
+        from: { opacity: 0, transform: 'scaleY(0)', x: -1000 },
+        enter: { opacity: 1, transform: 'scaleY(1)', x: 0 },
+    };
+    let transition = useTransition(null, props);
     return (
         <div id="main">
             <Header title={postData.title}>
             </Header>
-            <div id={styles.content} className="lg:mt-20 mx-auto mb-10">
+            {transition((props, _) => (
+            <animated.div style={props} id={styles.content} className="lg:mt-20 mx-auto mb-10">
                 <div className={`flex items-center`}>
                     <div className={`lg:w-1/2 mx-auto`}>
                         <h1 style={{textAlign: 'center'}} className={`text-3xl lg:text-5xl font-bold text-white mb-2 lg:mb-6 pt-3`}>{postData.title}</h1>
@@ -32,7 +39,8 @@ export default function Post({ postData, siteUrl }) {
                         <div id={styles.sub} className={`mx-auto`}><Sub siteUrl={siteUrl}></Sub></div>
                     </div>
                 </div>
-            </div>
+            </animated.div>
+            ))}
             <Footer></Footer>
         </div>
     );
