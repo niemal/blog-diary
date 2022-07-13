6df16cd5d80c022b8aad6991c38bbe79a9f4b430
niemal <niemal@gmx.com>
2022-07-14 00:07:43 +0300

    edge case for long-character tag inside a post

--- src/components/PostIndex/PostIndex.js
+++ src/components/PostIndex/PostIndex.js
@@ -108,7 +108,7 @@ const TagsPreWrapper = styled.div`
 
 const TagsWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
   gap: 16px;
 `;
 
